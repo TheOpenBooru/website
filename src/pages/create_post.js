c0dc88fb7b2cd0abee9b290fa7e0f9e79ea786d7
Ben Brady <benbradybusiness@gmail.com>
2022-02-19 22:52:54 +0000

    Fixed property names

--- src/pages/create_post.js
+++ src/pages/create_post.js
@@ -5,8 +5,8 @@ function CreatePost(props) {
     return (
         <div>
             <NavBar />
-            <form action="http://slate:5725/posts/create" method="post" enctype="multipart/form-data" target="_">
-                <label for="image_file ">Image:</label>
+            <form action="http://slate:5725/posts/create" method="post" encType="multipart/form-data" target="_">
+                <label htmlFor="image_file ">Image:</label>
                 <input type="file" id="image_file" name="image_file "/>
                 <br />
                 <input type="submit" value="Submit"/>
