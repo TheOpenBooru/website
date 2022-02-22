e3f190222876b121b9ff7e751a4661361a22e7e5
Ben Brady <benbradybusiness@gmail.com>
2022-02-22 23:51:37 +0000

    Updated Create post to not use Forms

--- src/pages/create_post.js
+++ src/pages/create_post.js
@@ -1,16 +1,30 @@
+import react from "react";
 import React from "react";
 import NavBar from "../components/navigation_bar";
 
+
 function CreatePost(props) {
+    let File_Input = react.createRef();
+    function handleSubmit(event) {
+        let files = File_Input.current.files;
+        for (const file of files) {
+            let form = new FormData();
+            form.append("image_file", file);
+            
+            let request = new XMLHttpRequest();
+            request.open("POST", "http://localhost:57255/posts/create");
+            request.send(form);
+        }
+    }
+
     return (
         <div>
             <NavBar />
-            <form action="http://slate:5725/posts/create" method="post" encType="multipart/form-data" target="_">
-                <label htmlFor="image_file ">Image:</label>
-                <input type="file" id="image_file" name="image_file "/>
-                <br />
-                <input type="submit" value="Submit"/>
-            </form>
+            <div>
+                <label htmlFor="image_file">Files:</label>
+                <input name="image_file" type="file" ref={File_Input} multiple/><br />
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
         </div>
     );
 }
