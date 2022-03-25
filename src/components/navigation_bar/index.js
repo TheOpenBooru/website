198eddd15e344a51b095ef5011ff627e9630b2d2
Ben Brady <benbradybusiness@gmail.com>
2022-03-25 21:46:40 +0000

    Migrated to page button component

--- src/components/navigation_bar/index.js
+++ src/components/navigation_bar/index.js
@@ -5,24 +5,24 @@ import "./nav_bar.css";
 
 function PageButton(props) {
     return (
-        <div className="navbar-section bordered" id={props.id}>
+        <a id={props.id} className={props.className} href={props.href}>
             <img className="navbar-button-icon" src={props.icon} alt="" />
             <span className="navbar-button-text" >{props.content}</span>
             {props.children}
-        </div>
+        </a>
     );
 }
 
 function NavBar() {
     return (
         <nav id="navigation-bar">
-            <a className="navbar-section bordered bordered-hover"
-                href="https://github.com/TheOpenBooru"  id="version-number">
-                <img className="navbar-button-icon" src="/images/github_logo.png" alt="" />
-                <text className="disable-link-text">Alpha 1</text>
-            </a>
-
-            <PageButton id="navbar-search" content="Search" icon="/images/posts.svg" >
+            <PageButton
+                className="navbar-section bordered bordered-hover"
+                content="Alpha 1" icon="/images/github_logo.png"
+                href="https://github.com/TheOpenBooru" id="version-number" />
+            
+            
+            <PageButton className="navbar-section bordered" id="navbar-search" content="Search" icon="/images/posts.svg" >
                 <PostSearch />
             </PageButton>
             <CreatePostBox className="navbar-section bordered"/>
