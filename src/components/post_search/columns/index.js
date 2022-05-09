0290bba3411b6483dc7f01820670fe7cb8a99235
Ben Brady <benbradybusiness@gmail.com>
2022-05-09 18:18:25 +0100

    Post Columns now distrobute posts

--- src/components/post_search/columns/index.js
+++ src/components/post_search/columns/index.js
@@ -3,12 +3,38 @@ import Item from "../item";
 import Redirects from "../../../js/redirects";
 import "./index.css";
 
+
+function get_minimum_column_height_index(columns) {
+    let bucket_heights = new Array(columns.length).fill(0);
+    columns.forEach((clmn, i) => {
+        let total = 0;
+        clmn.forEach((v) => total += v.full.height / v.full.width);
+        bucket_heights[i] = total;
+    })
+    let min_height = Math.min(...bucket_heights)
+    let index = bucket_heights.indexOf(min_height);
+    if (index === -1) {
+        return 0;
+    } else {
+        return index;
+    }
+}
+
 function SplitPosts(array, parts) {
     // Create an array of arrays
     // eslint-disable-next-line
-    let buckets = Array.apply(null, Array(parts)).map(() => Array());
-    array.forEach((v, i) => buckets[i % parts].push(v));
-    return buckets;
+    if (array === [] || array === undefined) {
+        return new Array(parts).fill([]);
+    }
+    else {
+        let buckets = Array.apply(null, Array(parts)).map(() => Array());
+        array.forEach((v) => {
+            let min_height_index = get_minimum_column_height_index(buckets)
+            console.log(min_height_index);
+            buckets[min_height_index].push(v)
+        });
+        return buckets;
+    }
 }
 
 export default function ColumnPosts(props) {
