a93292a120d5d68de5599ded8c88f7d1b0bc3ce5
Ben Brady <benbradybusiness@gmail.com>
2022-05-12 19:00:19 +0100

    Upgraded Posts Grid

--- src/components/post_search/grid/index.js
+++ src/components/post_search/grid/index.js
@@ -1,28 +1,40 @@
 import React from "react";
-import Item from "../item";
 import Redirects from "../../../js/redirects";
 import "./index.css";
 
+
+function GridItem(props) {
+    let { post } = props;
+    let className = `media-${post.media_type}`;
+    let redirect = Redirects.post(post.id);
+    let image = post.thumbnail;
+    return (
+        <a key={post.id} className={className} href={redirect}>
+            <img
+                src={image.url}
+                alt=""
+                width={image.width}
+                height={image.height}
+            />
+        </a>
+    );
+    
+}
 export default function GridPosts(props) {
     let { posts, posts_callback } = props;
 
     let scrollHandler = (e) => {
         const { scrollTop, offsetHeight, scrollHeight } = e.target;
-        if (scrollTop + offsetHeight >= scrollHeight) {
+        let distanceFromTop = scrollTop + offsetHeight;
+        let distanceFromBottom = scrollHeight - distanceFromTop;
+        if (distanceFromBottom < 2) {
             posts_callback();
         }
     };
 
     return (
         <div className="post-list" onScroll={scrollHandler}>
-            {posts.map((post, i) => (
-                <Item
-                    key={post.id}
-                    href={Redirects.post(post.id)}
-                    image={post.thumbnail}
-                    type={post.media_type}
-                />
-            ))}
+            {posts.map((post) => <GridItem key={post.id} post={post}/>)}
         </div>
     );
 }
