b0f7bd851fd9fa733a1a13b555f8a0f0507fd946
Ben Brady <benbradybusiness@gmail.com>
2022-06-08 21:47:36 +0100

    Removed TagList.css

--- src/components/PostSidebar/TagList.js
+++ src/components/PostSidebar/TagList.js
@@ -1,25 +1,32 @@
 import React from "react";
-import "./TagList.css"
+import Redirects from "js/redirects";
 
-function Tag(props) {
-    let namespace = props.namespace || "generic";
-    return <span className={`namespace-${namespace}`}>{props.name}</span>;
-}
-
-function TagList(props) {
+export default function TagList(props) {
     let tags = props.tags;
     if (!tags) {
         return <div id="postSidebar-tagList" />;
     } else {
         tags = tags.sort();
+
         return (
             <div id="postSidebar-tagList">
                 {tags.map((tag, i) => (
-                    <Tag key={i} name={tag} />
-                ))}
+                    <Tag key={i} tag={tag} />
+                    ))}
             </div>
         );
     }
 }
 
-export default TagList;
+function Tag(props) {
+    let { tag } = props;
+    function searchTagCallback() {
+        let queryJSON = window.sessionStorage.getItem("posts-search")
+        let query = JSON.parse(queryJSON);
+        query.include_tags.push(tag)
+        let encoded = JSON.stringify(query);
+        window.sessionStorage.setItem("posts-search", encoded)
+        Redirects.goto(Redirects.postSearch());
+    }
+    return <span className="postSidebar-tagList-tag" onClick={searchTagCallback}>{tag}</span>;
+}
