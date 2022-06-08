78843fb6d743cd5143ba9149a6b6024df7dff695
Ben Brady <benbradybusiness@gmail.com>
2022-06-08 19:43:26 +0100

    Removed test URL, Oops

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Post from "./containers/post";
 import Posts from "./containers/posts";
-import SearchBox from "./components/SearchBox";
 import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path="*" element={<Posts />} />
-            <Route path="/test" element={<SearchBox/>} />
             <Route path="/posts" element={<Posts />} />
             <Route path="/posts/:layout" element={<Posts />} />
             <Route path="/post/:id" element={<Post />} />
