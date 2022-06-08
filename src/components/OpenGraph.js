c7abfcc3ecbafb424c9e1581598e53b7e5ff8012
Ben Brady <benbradybusiness@gmail.com>
2022-06-08 16:44:29 +0100

    Reverted OpenGraph Helmet Changes

--- src/components/OpenGraph.js
+++ src/components/OpenGraph.js
@@ -4,7 +4,7 @@ import Helmet from "react-helmet";
 export default function OpenGraphTags(props) {
     let { title, description, image, video} = props;
     return (
-        <React.Fragment>
+        <Helmet>
             <meta property="og:site_name " content="Open Booru" />
             <meta property="og:url" content={document.location.href} />
             {title ? <meta property="og:title" content={title} /> : null}
@@ -12,6 +12,6 @@ export default function OpenGraphTags(props) {
             
             {image ? <meta property="og:image" content={image} /> : null}
             {video ? <meta property="og:video" content={video} /> : null}
-        </React.Fragment>
+        </Helmet>
     )
 }
