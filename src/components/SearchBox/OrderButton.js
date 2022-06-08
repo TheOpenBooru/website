fa7e30c6885a85eae235624c28670ec6f24acf0e
Ben Brady <benbradybusiness@gmail.com>
2022-06-08 21:46:44 +0100

    Updated Alt Text for the Orderbuton

--- src/components/SearchBox/OrderButton.js
+++ src/components/SearchBox/OrderButton.js
@@ -3,13 +3,14 @@ import React from "react";
 export default function OrderButton(props) {
     let { decending, setDecending } = props;
     let callback = () => setDecending(!decending)
+    let alt = decending ? "Sort Descending" : "Sort Ascending";
     return (
-        <div onClick={callback} className="center"> 
+        <div onClick={callback} className="center" title={alt}>
             <img
                 id="searchbox-order"
                 className="bordered"
                 src={decending ? "/images/arrow-down.svg" : "/images/arrow-up.svg"}
-                alt="Decending/Ascending"
+                alt={alt}
             />
         </div>
     )
