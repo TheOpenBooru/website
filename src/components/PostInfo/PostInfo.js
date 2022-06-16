4907576899646a9078158296f65362193fc68c5d
Ben Brady <benbradybusiness@gmail.com>
2022-06-16 16:50:07 +0100

    Source now displayed as hostname

--- src/components/PostInfo/PostInfo.js
+++ src/components/PostInfo/PostInfo.js
@@ -18,6 +18,12 @@ export default function PostInfo(props) {
     let { post } = props;
 
     let created_at = new Date(post.created_at * 1000).toLocaleDateString();
+    
+    let source;
+    if (post.source) {
+        let url = new URL(post.source)
+        source = url.hostname
+    }
     return (
         <div id="PostInfo-postData">
             <Entry name="ID" value={post.id} />
@@ -25,7 +31,11 @@ export default function PostInfo(props) {
             <Entry name="Views" value={post.views} />
             <Entry name="Upvotes" value={post.upvotes} />
             <Entry name="Downvotes" value={post.downvotes} />
-            <Entry name="Source" value={post.source} href={post.source} />
+            {post.source
+                ? <Entry name="Source" value={source} href={post.source} />
+                : <Entry name="Source" value="None" />
+            }
+            
         </div>
     );
 }
