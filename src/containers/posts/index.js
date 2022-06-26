97f7851f9ffcb5a2417dc103ac197c99329b4b2b
Ben Brady <benbradybusiness@gmail.com>
2022-06-26 13:28:55 +0100

    Updated MessageBox in Posts Overlay

--- src/containers/posts/index.js
+++ src/containers/posts/index.js
@@ -1,14 +1,11 @@
 import React, { useState,useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Core from "containers/core";
 import ColumnPosts from "components/ColumnPosts";
 import GridPosts from "components/GridPosts";
 import FullscreenPosts from "components/FullscreenPosts";
-import SearchBox from "components/SearchBox";
-import CreatePost from "components/CreatePost";
-import MessageBox from "components/MessageBox";
-import Core from "containers/core";
 import { PostSearch, PostQuery } from "js/booru";
-import ButtonOverlay from "./overlay";
+import Overlay from "./overlay";
 import "./search.css";
 
 function getQuery() {
@@ -22,68 +19,37 @@ function getQuery() {
 
 export default function Posts(props) {
     let { layout } = useParams();
-    let searchRef = React.useRef(null);
-    let createRef = React.useRef(null);
     let [ search, setSearch ] = useState(new PostSearch(getQuery()));
     let [ posts, setPosts ] = useState([]);
-    
+
     function setQuery(query) {
         window.sessionStorage.setItem("posts-search", JSON.stringify(query));
         setSearch(new PostSearch(query));
     }
-    
+
     useEffect(() => (async () => prepend_posts())(), [search]); // eslint-disable-line
-    
+
     async function prepend_posts() {
         await search.extend(100);
         setPosts(search.posts);
     }
     
     let LayoutLookup = {
-        "fullscreen": FullscreenPosts,
-        "grid": GridPosts,
-        "column": ColumnPosts,
-    }
+        fullscreen: FullscreenPosts,
+        grid: GridPosts,
+        column: ColumnPosts,
+    };
+    
     let PostsLayout = LayoutLookup[layout] ?? ColumnPosts;
-
-    function toggleSearchBox() {
-        if (searchRef.current) {
-            if (searchRef.current.style.display === "none") {
-                searchRef.current.style.display = "block";
-            } else {
-                searchRef.current.style.display = "none";
-            }
-        }
-    }
-
-    function toggleCreateBox() {
-        if (createRef.current) {
-            if (createRef.current.style.display === "none") {
-                createRef.current.style.display = "block";
-            } else {
-                createRef.current.style.display = "none";
-            }
-        }
-    }
 
     return (
         <Core title={`Open Booru: ${layout ? layout : "Post"} Search`}>
-            <div ref={searchRef} style={{ "display": "none" }}>
-                <MessageBox>
-                    <SearchBox query={search.query} setQuery={setQuery} />
-                </MessageBox>
-            </div>
-            <div ref={createRef} style={{ "display": "none" }}>
-                <MessageBox>
-                    <CreatePost/>
-                </MessageBox>
-            </div>
-            <ButtonOverlay toggleSearchBox={toggleSearchBox} toggleCreateBox={toggleCreateBox} />
-            {
-                search.finished && posts.length === 0 
-                    ? <span className="posts-ErrorText">No Posts Found</span>
-                    : <PostsLayout posts={posts} morePostsCallback={prepend_posts} />
-            }
+            <Overlay query={ search.query} setQuery={setQuery} />
+            {search.finished && posts.length === 0 ? (
+                <span className="posts-ErrorText">No Posts Found</span>
+            ) : (
+                <PostsLayout posts={posts} morePostsCallback={prepend_posts} />
+            )}
         </Core>
     );
 }
--- src/containers/posts/overlay.js
+++ src/containers/posts/overlay.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React,{ useRef } from "react";
+import MessageBox from "components/MessageBox";
+import SearchBox from "components/SearchBox";
+import CreatePost from "components/CreatePost";
 
 export default function PostOverlay(props) {
-    let { toggleSearchBox, toggleCreateBox } = props;
+    let { query, setQuery } = props;
+    let searchBoxRef = useRef(null);
+    let createBoxRef = useRef(null);
     
+    function toggleSearchBox() {
+        let searchBoxElem = searchBoxRef.current
+        let createBoxElem = createBoxRef.current
+        let isVisible = searchBoxElem.style.display === "none"
+        if (isVisible) {
+            createBoxElem.style.display = "none";
+            searchBoxElem.style.display = "unset";
+        } else {
+            searchBoxElem.style.display = "none";
+        }
+    }
+    
+    function toggleCreateBox() {
+        let searchBoxElem = searchBoxRef.current
+        let createBoxElem = createBoxRef.current
+        let isVisible = createBoxElem.style.display === "none"
+        if (isVisible) {
+            searchBoxElem.style.display = "none";
+            createBoxElem.style.display = "unset";
+        } else {
+            createBoxElem.style.display = "none";
+        }
+    }
+
     return (
         <React.Fragment>
+            <div ref={searchBoxRef} style={{display:"none"}}>
+                <MessageBox>
+                    <SearchBox query={query} setQuery={setQuery} />
+                </MessageBox>
+            </div>
+            <div ref={createBoxRef} style={{display:"none"}}>
+                <MessageBox >
+                    <CreatePost />
+                </MessageBox>
+            </div>
             <div id="posts-overlay">
                 <div className="posts-overlay-button" title="Create Post">
                     <img src="/images/plus.svg" alt="Create Post" onClick={toggleCreateBox} />
