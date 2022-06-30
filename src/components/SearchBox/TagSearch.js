9b1dda3831fe613ff8b32baff18196a27ba3bdf6
Ben Brady <benbradybusiness@gmail.com>
2022-06-30 15:03:09 +0100

    Improved Tag Search

--- src/components/SearchBox/TagSearch.js
+++ src/components/SearchBox/TagSearch.js
@@ -4,14 +4,14 @@ import "./tagSearch.css"
 
 export default function TagSearch(props) {
     let { includeTags, setIncludeTags } = props;
-    let [ predictedTags, setPredictedTags ] = useState([]);
-    let inputRef = useRef();
-
+    let [predictedTags, setPredictedTags] = useState([]);
+    let [ text, setText] = useState("");
+    
     function addTagCallback(tag) {
         return () => {
             tag = tag.toLowerCase();
             setPredictedTags([]);
-            inputRef.current.value = "";
+            setText("")
             if (!includeTags.includes(tag)) {
                 setIncludeTags(includeTags.concat([tag]));
             }
@@ -19,20 +19,23 @@ export default function TagSearch(props) {
     }
 
     async function loadPredictedTags(text) {
-        let query = new TagQuery();
-        query.name_like = text;
-        query.limit = 5;
-        let tags = await Tags.search(query)
-        setPredictedTags(tags);
-    }
-
-    function tagChange(e) {
-        let text = e.target.value;
         if (text.length === 0) {
             setPredictedTags([]);
         } else {
-            loadPredictedTags(text);
+            let query = new TagQuery();
+            query.name_like = text;
+            query.limit = 7;
+            let tags = await Tags.search(query)
+            setPredictedTags(tags);
         }
+    }
+
+    function onInput(e) {
+        let text = e.target.value;
+        text = text.replace(' ','_')
+        text = text.toLowerCase();
+        setText(text);
+        loadPredictedTags(text);
     }
 
     return (
@@ -40,8 +43,8 @@ export default function TagSearch(props) {
             <input
                 id="searchbox-search"
                 type="search"
-                ref={inputRef}
-                onChange={tagChange}
+                value={text}
+                onChange={onInput}
                 onKeyDown={addTagCallback}
             />
             {predictedTags.length === 0 ? null : (
