0bd9ce4a42cb815229cd2c50a0f45004101a8a5c
Ben Brady <benbradybusiness@gmail.com>
2022-09-04 22:02:03 +0100

    Updated About Us

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,12 +5,18 @@ import styled from "styled-components";
 export default function About() {
     return (
         <div>
-            OpenBooru is a modern booru framework written in Python attempting to supliment existing implementations.
-            <br/>
+            Open Booru is a modern booru framework designed to replace existing implementations and allow for the creation of custom clients.
+            <br />
+            <br />
+            Source Code:&nbsp;
+            <Link href="https://github.com/TheOpenBooru">
+                github.com/TheOpenBooru
+            </Link>
+            {/* <br/>
             Sister Site:
             <Link href={"https://r34.pics"}>
                 r34.pics
-            </Link>
+            </Link> */}
         </div>
     );
 }
