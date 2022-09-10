ab5fb65a2f0d00f693989a1b08d586cabb0cc38f
Ben Brady <benbradybusiness@gmail.com>
2022-09-10 18:37:05 +0100

    Updated config

--- next.config.js
+++ next.config.js
@@ -6,51 +6,58 @@ const nextConfig = {
     compiler: {
         styledComponents: {
             ssr: true,
-        }
+        },
     },
     experimental: {
         images: {
-            allowFutureImage: true
-        }
+            allowFutureImage: true,
+        },
     },
     images: {
         domains: [
-            "safebooru.org", "static1.e621.net",
-            "slate", "localhost", "192.168.0.82", 
-            "r34proxy.openbooru.workers.dev", "cdn.openbooru.org", "api.openbooru.org",
-        ]
+            "safebooru.org",
+            "static1.e621.net",
+            "slate",
+            "localhost",
+            "192.168.0.82",
+            "r34proxy.openbooru.workers.dev",
+            "cdn.openbooru.org",
+            "api.openbooru.org",
+        ],
     },
     env: {
         SITE_NAME: "Open Booru",
-        SITE_DESCRIPTION: "The Open Booru, the free and open-source image board",
-        SITE_URL: "https://www.openbooru.org",
+        SITE_DESCRIPTION: "Openbooru, the free and open-source image board based on Gelbooru and Danbooru.",
+        SITE_URL: "https://openbooru.org",
         API_URL: "https://api.openbooru.org",
         SERVER_API_KEY: null,
         READ_ONLY: false,
         PROMPT_AGE: false,
     },
     webpack(config, options) {
-        return config
+        return config;
     },
     async headers() {
-        return [{
-            source: "/:path*",
-            headers: [
-                {
-                    key:"X-Frame-Options",
-                    value:"DENY",
-                },
-                {
-                    key:"X-Content-Type-Options",
-                    value:"nosniff",
-                },
-                {
-                    key:"Referrer-Policy",
-                    value:"strict-origin-when-cross-origin",
-                },
-            ]
-        }]
-    }
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "strict-origin-when-cross-origin",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
